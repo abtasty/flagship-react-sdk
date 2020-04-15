0ed4c1ec61faebc30ee619d94be803a1f8cd889c
Emilien Domenge <emilien.domenge-heritier@abtasty.com>
2020-04-15 17:09:15 +0200

    Updates

---
+++ precache-manifest.cfd93b7bc56104212b12b01b1818c418.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824c0dc6e57ca4b3753db4875abca363",
+    "url": "/flagship-react-sdk/index.html"
+  },
+  {
+    "revision": "c3b74a58a89a88b98ced",
+    "url": "/flagship-react-sdk/static/css/2.131ee16b.chunk.css"
+  },
+  {
+    "revision": "7e2c18c50339b01c505d",
+    "url": "/flagship-react-sdk/static/css/main.03c78fc5.chunk.css"
+  },
+  {
+    "revision": "c3b74a58a89a88b98ced",
+    "url": "/flagship-react-sdk/static/js/2.903cec8f.chunk.js"
+  },
+  {
+    "revision": "7e2c18c50339b01c505d",
+    "url": "/flagship-react-sdk/static/js/main.d234b33a.chunk.js"
+  },
+  {
+    "revision": "1e047d4f689753788c5a",
+    "url": "/flagship-react-sdk/static/js/runtime-main.b0c93c95.js"
+  },
+  {
+    "revision": "6e8b90f343141007a9efaca1691e2591",
+    "url": "/flagship-react-sdk/static/media/Flagship-horizontal-product-white.6e8b90f3.png"
+  }
+]);
